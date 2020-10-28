02d086910c4110cb1009b06d9c8ae43a62acbbd7
Corentin Nélias <corentin.nelias@gmail.com>
2020-10-28 13:48:10 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using CategoricalTimeSeries
 
 #testing the serial dependences functions by reproducing the results from C. Weiss's book "An Introduction to Discrete-Valued Time Series".
 #I was not able to get my hands the full time-series, C. weiss says it is ~1300 elements long, which explains the slight numerical differences in values.
-pewee = Int64.(readdlm("pewee.txt", ',')[1,:])
+pewee = Int64.(readdlm("pewee")[:,1])
 @test round(cramer_coefficient(pewee, 4)[1], digits = 2) == 0.46
 @test round(cohen_coefficient(pewee, 4)[1], digits = 2) == 0.55
 #this one was not in the book, but the plot correctly reproduces the results.
