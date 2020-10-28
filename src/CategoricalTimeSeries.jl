094db84875d7d508a0df9cdc625f5e6b9cd33307
Corentin Nélias <corentin.nelias@gmail.com>
2020-10-28 13:37:00 +0100

    Update CategoricalTimeSeries.jl

--- src/CategoricalTimeSeries.jl
+++ src/CategoricalTimeSeries.jl
@@ -4,4 +4,8 @@ using IntegerIB
 using SerialDependence
 using SpectralEnvelope
 
+export IB, search_optima!, brute_optimize!, IB_optimize!, calc_metrics, get_IB_curve, get_y, print_results
+export cramer_coefficient, cohen_coefficient, conditional_entropy, LaggedBivariateProbability, H, theils_u, rate_evolution
+export spectral_envelope, get_mappings, detrend, smooth, power_spectrum
+
 end
