2d33170876f3a317a14097d2130512d74021846a
Corentin Nélias <corentin.nelias@gmail.com>
2021-01-15 17:17:55 +0100

    Update CategoricalTimeSeries.jl

--- src/CategoricalTimeSeries.jl
+++ src/CategoricalTimeSeries.jl
@@ -3,9 +3,12 @@ module CategoricalTimeSeries
 using IntegerIB
 using SerialDependence
 using SpectralEnvelope
+using MotifRecognition
 
 export IB, search_optima!, brute_optimize!, IB_optimize!, calc_metrics, get_IB_curve, get_y, print_results
 export cramer_coefficient, cohen_coefficient, conditional_entropy, LaggedBivariateProbability, H, theils_u, rate_evolution
 export spectral_envelope, get_mappings, detrend, smooth, power_spectrum
+export detect_motifs, mapping, find_motifs, plot_motif, expected_matches, least_occurence_threshold
+
 
 end
