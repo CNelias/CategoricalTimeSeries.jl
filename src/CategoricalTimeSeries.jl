f895e5624fa8ea62c40c03ac8999963e6aa9bd76
Corentin Nélias <corentin.nelias@gmail.com>
2021-09-29 12:51:17 +0200

    Update CategoricalTimeSeries.jl

--- src/CategoricalTimeSeries.jl
+++ src/CategoricalTimeSeries.jl
@@ -6,7 +6,7 @@ using SpectralEnvelope
 include("MotifRecognition.jl")
 
 export IB, search_optima!, brute_optimize!, IB_optimize!, calc_metrics, get_IB_curve, get_y, print_results
-export cramer_coefficient, cohen_coefficient, conditional_entropy, LaggedBivariateProbability, H, theils_u, rate_evolution
+export cramer_coefficient, cohen_coefficient, conditional_entropy, LaggedBivariateProbability, H, theils_u, rate_evolution, bootstrap_CI
 export spectral_envelope, get_mappings, detrend, smooth, power_spectrum
 export detect_motifs, mapping, find_motifs, plot_motif, expected_matches, least_occurence_threshold
 
