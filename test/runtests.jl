71078a63600496e97b5f8eaa179102d703c84bc0
Corentin Nélias <corentin.nelias@gmail.com>
2021-09-30 11:50:23 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ using CategoricalTimeSeries
 #I was not able to get my hands the full time-series, C. weiss says it is ~1300 elements long, which explains the slight numerical differences in values.
 @testset "AssociationMeasurement" begin
   pewee = Int64.(readdlm("pewee.txt", ',')[1,:])
-  @test round(cramer_coefficient(pewee, 4)[1], digits = 2) == 0.46
-  @test round(cohen_coefficient(pewee, 4)[1], digits = 2) == 0.55
+  @test round(cramer_coefficient(pewee, Int(4))[1], digits = 2) == 0.46
+  @test round(cohen_coefficient(pewee, Int(4))[1], digits = 2) == 0.55
   #this one was not in the book, but the plot correctly reproduces the results.
   @test round(theils_u(pewee,[1])[1], digits = 2) == 0.55
   @test round(H(pewee), digits = 2) == 1.49
