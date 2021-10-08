3c8f34e7199486a68b96319d083881c3a57d98f7
Corentin Nélias <corentin.nelias@gmail.com>
2021-10-08 18:02:10 +0200

    Adding test for each possible input type

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using Test
 using DelimitedFiles
 using CategoricalTimeSeries
 
+#toy time-series presenting one predictable pattern "b" -> "a"
+test_ts_string = ["a", "b", "a", "b", "a", "c", "d", "b", "a", "a", "d", "b", "a", "b", "a", "d", "c", "d", "b", "a", "d", "a", "c", "b", "a", "a", "b", "a", "c", "b", "a"]
+test_ts_integer = [1, 2, 1, 2, 1, 3, 4, 2, 1, 1, 4, 2, 1, 2, 1, 4, 3, 4, 2, 1, 4, 1, 3, 2, 1, 1, 2, 1, 3, 2, 1] 
+test_ts_float = [1.1, 2.1, 1.1, 2.1, 1.1, 3.1, 4.1, 2.1, 1.1, 1.1, 4.1, 2.1, 1.1, 2.1, 1.1, 4.1, 3.1, 4.1, 2.1, 1.1, 4.1, 1.1, 3.1, 2.1, 1.1, 1.1, 2.1, 1.1, 3.1, 2.1, 1.1] 
+
+
 #testing the serial dependences functions by reproducing the results from C. Weiss's book "An Introduction to Discrete-Valued Time Series".
 #I was not able to get my hands the full time-series, C. weiss says it is ~1300 elements long, which explains the slight numerical differences in values.
 @testset "AssociationMeasurement" begin
@@ -12,6 +18,15 @@ using CategoricalTimeSeries
   #this one was not in the book, but the plot correctly reproduces the results.
   @test round(theils_u(pewee,[1])[1], digits = 2) == 0.55
   @test round(H(pewee), digits = 2) == 1.49
+  #testing if integer and float input are also working
+  @test round(cramer_coefficient(test_ts_integer, Int64(4))[1], digits = 2) == 0.04
+  @test round(cramer_coefficient(test_ts_float, Int64(4))[1], digits = 2) == 0.04
+  @test round(cohen_coefficient(test_ts_integer, Int64(4))[1], digits = 2) == -0.04
+  @test round(cohen_coefficient(test_ts_float, Int64(4))[1], digits = 2) == -0.04
+  @test round(theils_u(test_ts_integer, Int64(4))[1], digits = 2) == 0.13
+  @test round(theils_u(test_ts_float, Int64(4))[1], digits = 2) == 0.13 
+  @test round(H(test_ts_integer), digits = 2) == 1.85
+  @test round(H(test_ts_float), digits = 2) == 1.85 
 end
 
 @testset "SpectralEnvelope" begin
@@ -22,6 +37,9 @@ end
   @test round(spectral_envelope(DNA)[2][5]; digits = 3) == round(0.003; digits = 3)
   mappings = get_mappings(DNA, 0.33)
   @test round(mappings["A"]; digits = 2) == round(0.54; digits = 2)
+  #testing if integer and floats are also working as input
+  round(spectral_envelope(test_ts_integer)[2][1]; digits = 2) == round(0.08; digits = 2)
+  round(spectral_envelope(test_ts_float)[2][1]; digits = 2) == round(0.08; digits = 2)
 end
 
 #testing integerIB
@@ -37,13 +55,17 @@ end
   @test round(ixt, digits = 2) == 2.06
   @test round(iyt, digits = 2) == 0.78
   @test round(L, digits = 2) == 1.32
-  #toy time-series presenting one predictable pattern "b" -> "a"
-  test_ts = ["a", "b", "a", "b", "a", "c", "d", "b", "a", "a", "d", "b", "a", "b", "a", "d", "c", "d", "b", "a", "d", "a", "c", "b", "a", "a", "b", "a", "c", "b", "a"]
-  model = IB(test_ts, 500)
+  model = IB(test_ts_string, 500) #testing string input
   IB_optimize!(model)
-  print("Final cluster size:" , size(model.qt_x, 1))
-  @test (size(model.qt_x, 1) == 2 || size(model.qt_x, 1) == 3 || size(model.qt_x, 1) == 4) #we expect the algorithm to cluster at least 2 labels together.
+  #we expect the algorithm to cluster at least 2 labels together.
   #However, due to the probabilistic nature of the algorithm it is possible that no clustering comes out (~0.5% chance) hence the allowance of ```size(model.qt_x, 1) == 4```
+  @test (size(model.qt_x, 1) == 2 || size(model.qt_x, 1) == 3 || size(model.qt_x, 1) == 4)  
+  model = IB(test_ts_integer , 500) #same test with integer input
+  IB_optimize!(model)
+  @test (size(model.qt_x, 1) == 2 || size(model.qt_x, 1) == 3 || size(model.qt_x, 1) == 4) 
+  model = IB(test_ts_float, 500) #same test with float input
+  IB_optimize!(model)
+  @test (size(model.qt_x, 1) == 2 || size(model.qt_x, 1) == 3 || size(model.qt_x, 1) == 4) 
 end
 
 #testing motif recognition
@@ -57,5 +79,7 @@ end
   consensus_shape = m[1].shape
   similar_motifs = find_motifs(intervals, consensus_shape, 1)
   @test similar_motifs.instances[1] ==  [-1.0, -2.0, 10.0, -10.0, 2.0, 3.0, 5.0]
+  #testing that the functions also works with Array{String, 1} input
+  @test detect_motifs(test_ts_string, 5, 1)[1].shape == ["d", "b", "a", "b", "a"]
 end
 
