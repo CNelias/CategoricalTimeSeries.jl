3054b4ecaa70062c8ed15cc5a28249de5eeed418
Corentin Nélias <corentin.nelias@gmail.com>
2021-10-08 14:44:00 +0200

    export ```apply_mappings```

--- src/CategoricalTimeSeries.jl
+++ src/CategoricalTimeSeries.jl
@@ -7,7 +7,7 @@ include("MotifRecognition.jl")
 
 export IB, search_optima!, brute_optimize!, IB_optimize!, calc_metrics, get_IB_curve, get_y, print_results
 export cramer_coefficient, cohen_coefficient, conditional_entropy, LaggedBivariateProbability, H, theils_u, rate_evolution, bootstrap_CI
-export spectral_envelope, get_mappings, detrend, smooth, power_spectrum
+export spectral_envelope, get_mappings, detrend, smooth, power_spectrum, apply_mappings
 export detect_motifs, mapping, find_motifs, plot_motif, expected_matches, least_occurence_threshold
 
 end
